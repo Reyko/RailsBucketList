e597d17a8c5355bed60391cbe8cee8121a04789f
Serafeim Maroulis <seramak@gmail.com>
2014-05-29 22:34:18 +0100

    maps are now optimized

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,18 +1,16 @@
 var map;
-var toDoLat;
-var toDoLong;
 var marker;
 
-$(document).ready(function() {
+var cool_beans = function() {
   
   var destinations = document.getElementsByClassName("map-canvas");
 
   _(destinations).each(function(destination){
-
+    
     console.log(destination.id);
-
-    var latitude = document.getElementById("latitude " + destination.id).innerHTML;
-    var longitude = document.getElementById("longitude " + destination.id).innerHTML;
+  
+    var latitude = $("#" + destination.id).data("latitude");
+    var longitude = $("#" + destination.id).data("longitude");
 
     var options = {
     zoom:8,
@@ -21,28 +19,29 @@ $(document).ready(function() {
     };
 
     map = new google.maps.Map($("#"+destination.id)[0],options);
-
-
-
     var toDoItems = document.getElementsByClassName("to-do-item");
-
-
 
    _(toDoItems).each(function(toDoItem){
 
-    if(document.getElementById("toDoLat " + toDoItem.id) != null && document.getElementById("toDoLong " + toDoItem.id) != null){
+      if(document.getElementById("toDoLat " + toDoItem.id) != null && document.getElementById("toDoLong " + toDoItem.id) != null){
 
-      toDoLat = document.getElementById("toDoLat " + toDoItem.id).innerHTML;
-      toDoLong = document.getElementById("toDoLong " + toDoItem.id).innerHTML;
-
-      
-       addMarker(toDoLat,toDoLong);
-      
-     }
+        var toDoLat = document.getElementById("toDoLat " + toDoItem.id).innerHTML;
+        var toDoLong = document.getElementById("toDoLong " + toDoItem.id).innerHTML;
+        addMarker(toDoLat,toDoLong);
+        
+      }
     });    
   });
-});
+}
 
+
+$(document).ready(cool_beans);
+
+$(function() {
+    $("#button").click(function() {
+        setTimeout(cool_beans, 1000);
+    });
+});
 
 function addMarker(lat,lng) {
   marker = new google.maps.Marker({
